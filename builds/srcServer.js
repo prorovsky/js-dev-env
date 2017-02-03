53d0c284d72b3cb00257b8a81a4dc2941507bb03
Pavel Prorovsky <prorovsky@gmail.com>
2017-02-03 15:56:52 +0300

    add mock api

--- builds/srcServer.js
+++ builds/srcServer.js
@@ -21,6 +21,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', (req, res) => {
+    res.json([
+        {"id": 1, "firstName":"Bob", "lastName":"Smith", "email":"bob@gmail.com"},
+        {"id": 2, "firstName":"Marine", "lastName":"Green", "email":"marine@gmail.com"},
+        {"id": 3, "firstName":"Jack", "lastName":"Black", "email":"jack@gmail.com"}
+    ]);
+});
+
 app.listen(port, (err) => {
     if(err){
         console.error(err);
